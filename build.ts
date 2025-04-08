e2f363d3f883f0e8869f7406b2033c1aed70f46c
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2025-04-08 08:46:36 +0200

    Use switch

--- build.ts
+++ build.ts
@@ -1,19 +1,14 @@
-import fs from "fs";
-import os from "os";
-import path from "path";
-import util from "util";
-
 import { PromisePool } from "@supercharge/promise-pool";
-const findRoot = require("find-root");
-
+import findRoot from "find-root";
+import fs from "node:fs";
+import os from "node:os";
+import path from "node:path";
+import util from "node:util";
 import packageInfo from "./package.json";
 
-const langArg = process.argv[2];
-
 const exec = util.promisify(require("child_process").exec);
-
+const langArg = process.argv[2];
 const outDir = path.join(__dirname, "out");
-
 let hasErrors = false;
 
 async function buildParserWASM(
@@ -29,9 +24,9 @@ async function buildParserWASM(
     cliPackagePath = path.join(__dirname, "node_modules", "tree-sitter-cli");
   }
 
-  let cliPath = path.join(cliPackagePath, "tree-sitter");
-  let generateCommand = cliPath.concat(" generate");
-  let buildCommand = cliPath.concat(" build --wasm");
+  const cliPath = path.join(cliPackagePath, "tree-sitter");
+  const generateCommand = cliPath.concat(" generate");
+  const buildCommand = cliPath.concat(" build --wasm");
 
   try {
     console.log(`⏳ Building ${label}`);
@@ -54,58 +49,53 @@ async function buildParserWASM(
   }
 }
 
-if (fs.existsSync(outDir)) {
-  fs.rmSync(outDir, { recursive: true, force: true });
+function buildParserWASMS() {
+  const grammars = Object.keys(packageInfo.devDependencies)
+    .filter(
+      (n) =>
+        n.startsWith("tree-sitter-") &&
+        n !== "tree-sitter-cli" &&
+        n !== "tree-sitter"
+    )
+    .filter((s) => !langArg || s.includes(langArg));
+
+  return PromisePool.withConcurrency(os.cpus().length)
+    .for(grammars)
+    .process(async (name: string) => {
+      switch (name) {
+        case "tree-sitter-php":
+          await buildParserWASM(name, { subPath: "php" });
+          break;
+        case "tree-sitter-typescript":
+          await buildParserWASM(name, { subPath: "typescript" });
+          await buildParserWASM(name, { subPath: "tsx" });
+          break;
+        case "tree-sitter-xml":
+          await buildParserWASM(name, { subPath: "xml" });
+          await buildParserWASM(name, { subPath: "dtd" });
+          break;
+        case "tree-sitter-markdown":
+          await buildParserWASM(name, { subPath: "tree-sitter-markdown" });
+          await buildParserWASM(name, {
+            subPath: "tree-sitter-markdown-inline",
+          });
+          break;
+        case "tree-sitter-perl":
+        case "tree-sitter-latex":
+        case "tree-sitter-swift":
+          await buildParserWASM(name, { generate: true });
+          break;
+        default:
+          await buildParserWASM(name);
+      }
+    });
 }
 
 fs.mkdirSync(outDir);
-
 process.chdir(outDir);
 
-const grammars = Object.keys(packageInfo.devDependencies)
-  .filter(
-    (n) =>
-      n.startsWith("tree-sitter-") &&
-      n !== "tree-sitter-cli" &&
-      n !== "tree-sitter"
-  )
-  .filter((s) => !langArg || s.includes(langArg));
-
-PromisePool.withConcurrency(os.cpus().length)
-  .for(grammars)
-  .process(async (name: string) => {
-    if (name == "tree-sitter-agda") {
-      await buildParserWASM(name);
-    } else if (name == "tree-sitter-perl") {
-      await buildParserWASM(name, { generate: true });
-    } else if (name == "tree-sitter-php") {
-      await buildParserWASM(name, { subPath: "php" });
-    } else if (name == "tree-sitter-typescript") {
-      await buildParserWASM(name, { subPath: "typescript" });
-      await buildParserWASM(name, { subPath: "tsx" });
-    } else if (name == "tree-sitter-latex") {
-      await buildParserWASM(name, { generate: true });
-    } else if (name == "tree-sitter-xml") {
-      await buildParserWASM(name, { subPath: "xml" });
-      await buildParserWASM(name, { subPath: "dtd" });
-    } else if (name == "tree-sitter-query") {
-      await buildParserWASM(name);
-    } else if (name == "tree-sitter-elixir") {
-      await buildParserWASM(name);
-    } else if (name == "tree-sitter-markdown") {
-      await buildParserWASM(name, { subPath: "tree-sitter-markdown" });
-      await buildParserWASM(name, { subPath: "tree-sitter-markdown-inline" });
-    } else if (name === "tree-sitter-swift") {
-      await buildParserWASM(name, { generate: true });
-    } else {
-      await buildParserWASM(name);
-    }
-  })
-  .then(async () => {
-    if (hasErrors) {
-      //not sure if this failsafe is actually required, but it doesn't hurt to ensure that no wasms can be published from a failed build
-      fs.rmSync(outDir, { recursive: true, force: true });
-      fs.mkdirSync(outDir);
-      process.exit(1);
-    }
-  });
+buildParserWASMS().then(() => {
+  if (hasErrors) {
+    process.exit(1);
+  }
+});

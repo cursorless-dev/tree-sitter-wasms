315de3ff8984308546e8ce8459690fd22760bd80
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2025-04-08 08:28:06 +0200

    Clean up build script

--- build.ts
+++ build.ts
@@ -16,40 +16,6 @@ const outDir = path.join(__dirname, "out");
 
 let hasErrors = false;
 
-async function gitCloneOverload(
-  name: string,
-  repoUrl: string,
-  useLatest: boolean,
-  { commitHash }: { commitHash?: string }
-) {
-  let packagePath;
-  try {
-    packagePath = findRoot(require.resolve(name));
-  } catch (_) {
-    packagePath = path.join(__dirname, "node_modules", name);
-  }
-
-  let commitInfo = commitHash === undefined ? "latest" : commitHash;
-
-  try {
-    console.log(`🗑️  Deleting cached node dep for ${name}`);
-    await exec(`rm -rf ${packagePath}`);
-    console.log(`⬇️  Cloning ${name} from git (${commitInfo})`);
-    await exec(`git clone ${repoUrl} ${packagePath}`);
-    if (!useLatest) {
-      if (commitHash !== undefined) {
-        process.chdir(packagePath);
-        await exec(`git reset --hard ${commitHash}`);
-      } else
-        throw new Error(
-          "Latest commit is not being used, yet no commit hash was specified"
-        );
-    }
-  } catch (err) {
-    console.error(`❗Failed to clone git repo for ${name}:\n`, err);
-  }
-}
-
 async function buildParserWASM(
   name: string,
   { subPath, generate }: { subPath?: string; generate?: boolean } = {}
@@ -109,20 +75,8 @@ PromisePool.withConcurrency(os.cpus().length)
   .for(grammars)
   .process(async (name: string) => {
     if (name == "tree-sitter-agda") {
-      await gitCloneOverload(
-        name,
-        "https://github.com/tree-sitter/tree-sitter-agda.git",
-        false,
-        { commitHash: "47802091de0cb8ac2533d67ac37e65692c5902c4" }
-      );
       await buildParserWASM(name);
     } else if (name == "tree-sitter-perl") {
-      await gitCloneOverload(
-        name,
-        "https://github.com/tree-sitter-perl/tree-sitter-perl.git",
-        false,
-        { commitHash: "b467c41aed6e52c7f581c18a3b50821995a82c95" }
-      );
       await buildParserWASM(name, { generate: true });
     } else if (name == "tree-sitter-php") {
       await buildParserWASM(name, { subPath: "php" });
@@ -135,28 +89,10 @@ PromisePool.withConcurrency(os.cpus().length)
       await buildParserWASM(name, { subPath: "xml" });
       await buildParserWASM(name, { subPath: "dtd" });
     } else if (name == "tree-sitter-query") {
-      await gitCloneOverload(
-        name,
-        "https://github.com/tree-sitter-grammars/tree-sitter-query.git",
-        false,
-        { commitHash: "a6674e279b14958625d7a530cabe06119c7a1532" }
-      );
       await buildParserWASM(name);
     } else if (name == "tree-sitter-elixir") {
-      await gitCloneOverload(
-        name,
-        "https://github.com/elixir-lang/tree-sitter-elixir.git",
-        false,
-        { commitHash: "02a6f7fd4be28dd94ee4dd2ca19cb777053ea74e" }
-      );
       await buildParserWASM(name);
     } else if (name == "tree-sitter-markdown") {
-      await gitCloneOverload(
-        name,
-        "https://github.com/tree-sitter-grammars/tree-sitter-markdown",
-        false,
-        { commitHash: "192407ab5a24bfc24f13332979b5e7967518754a" }
-      );
       await buildParserWASM(name, { subPath: "tree-sitter-markdown" });
       await buildParserWASM(name, { subPath: "tree-sitter-markdown-inline" });
     } else if (name === "tree-sitter-swift") {

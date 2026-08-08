b3bf0f65ff2d1449d17a90d14690678ed07442c0
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2026-08-08 14:27:45 +0200

    Try with out sha overload

--- build.ts
+++ build.ts
@@ -121,16 +121,17 @@ async function processParser(name: ParserName) {
       });
       break;
 
-    case "tree-sitter-swift":
-      // This is a crude bodge job that should be replaced if possible in the future!
-      // You need to get the commit hash from the with-generated-files branch in the tree-sitter-swift repo.
-      await gitCloneOverload(name, "31d17fe7e818a2048c808b5c6fdc2dc792f4f5b5");
-      await buildParserWASM(name);
-      break;
+    // case "tree-sitter-swift":
+    //   // This is a crude bodge job that should be replaced if possible in the future!
+    //   // You need to get the commit hash from the with-generated-files branch in the tree-sitter-swift repo.
+    //   await gitCloneOverload(name, "31d17fe7e818a2048c808b5c6fdc2dc792f4f5b5");
+    //   await buildParserWASM(name);
+    //   break;
 
     case "tree-sitter-elixir":
     case "tree-sitter-perl":
     case "tree-sitter-query":
+    case "tree-sitter-swift":
       await gitCloneOverload(name);
       await buildParserWASM(name, { generate: true });
       break;
